106b436db533d58ea6ae4e37ef8953e9c6c64857
arquitechthor <arquitechthor@gmail.com>
2024-01-22 16:09:29 -0400

    Added: Service Test

--- src/main/java/com/arquitechthor/hexashop/domain/Product.java
+++ src/main/java/com/arquitechthor/hexashop/domain/Product.java
@@ -1,10 +1,12 @@
 package com.arquitechthor.hexashop.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
+@AllArgsConstructor
 public class Product {
 
     private Long id;
